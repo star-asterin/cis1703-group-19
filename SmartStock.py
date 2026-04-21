92a58a7354e70f881033b328b79c7b9736bfe626
Angel Prus <lilacghostiegaming@gmail.com>
2026-04-21 10:48:28 +0100

    Started adding the beginning part of the GUI

--- SmartStock.py
+++ SmartStock.py
@@ -1,1 +1,9 @@
 import tkinter as tk
+
+class SmartStockUI:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("SmartStock Terminal")
+
+self.root.geometry("1000x600")
+self.root.configure(bg= "#121212")
