4d6925873205714e7a2acf9f183a3c1c7070095f
haisakataiga <haillietiger@gmail.com>
2026-05-08 18:20:24 +0100

    Edited listbox scrollbar for x and y-axis

--- SmartStock.py
+++ SmartStock.py
@@ -258,14 +258,21 @@ def add_stock():
     
 add_window_button = tk.Button(btn_frame, text="Add Stock", command=add_stock_window).grid(row=0, column=0, padx=3)
 
-scrollbar = tk.Scrollbar(root, orient="vertical")
+frame = tk.Frame(root, width=40, height=15)
+frame.pack(pady=10)
+
+scrollbar = tk.Scrollbar(frame, orient="vertical")
 scrollbar.pack(side="right", fill="y")
 
+sidescroll = tk.Scrollbar(frame, orient="horizontal")
+sidescroll.pack(side="bottom", fill="x")
+
 #Listbox to display stock items
-stock_list = tk.Listbox(root, width=40, height=10, font=default_font, yscrollcommand=scrollbar.set)
-stock_list.pack(pady=10)
+stock_list = tk.Listbox(frame, width=40, height=15, font=default_font, yscrollcommand=scrollbar.set, xscrollcommand=sidescroll.set)
+stock_list.pack()
 
 scrollbar.config(command=stock_list.yview)
+sidescroll.config(command=stock_list.xview)
 
 
 #Function to remove selected stock from the list (Angel)
