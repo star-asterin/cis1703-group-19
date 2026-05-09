92591356234ce25aa1b8fec35d6a7d1242d3ad69
Thomalexc <thomalexcopeland@gmail.com>
2026-05-09 12:52:32 +0100

    changed all objects to ttk

--- SmartStock.py
+++ SmartStock.py
@@ -4,6 +4,7 @@
 #Add a section for value calculations as well as transaction history
 
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 import json
 from datetime import datetime
@@ -16,7 +17,7 @@ default_font = ("Arial", 12)
 label_font = ("Arial", 11)
 
 #Status Label
-status=tk.Label(root, text="", font=("Arial",10))
+status=ttk.Label(root, text="", font=("Arial",10))
 status.pack()
 # base class
 class Product:
@@ -56,7 +57,7 @@ class Counter:
 
 counter = Counter()
 
-btn_frame = tk.Frame(root)
+btn_frame = ttk.Frame(root)
 btn_frame.pack(pady=5)
 
 #Stock add function with an alert for empty input box- Angel
@@ -77,75 +78,75 @@ def add_stock_window():
     opt = tk.StringVar(add_window)
     opt.set("Default Product")
 
-    class DefaultOptions(tk.Frame):
+    class DefaultOptions(ttk.Frame):
         def __init__(self, parent):
             super().__init__(parent)
             global stock_name
-            stock_name = tk.Entry(self, width=32, font=default_font)
+            stock_name = ttk.Entry(self, width=32, font=default_font)
             global stock_price
-            stock_price = tk.Entry(self, width=32, font=default_font)
+            stock_price = ttk.Entry(self, width=32, font=default_font)
             global stock_quantity
-            stock_quantity = tk.Entry(self, width=32, font=default_font)
-            tk.Label(
+            stock_quantity = ttk.Entry(self, width=32, font=default_font)
+            ttk.Label(
                 self, 
                 text="Name of item",
                 font=label_font).pack(pady=3)
             stock_name.pack(pady=3)
-            tk.Label(
+            ttk.Label(
                 self, 
                 text="Price of item",
                 font=label_font).pack(pady=3)
             stock_price.pack(pady=3)
-            tk.Label(
+            ttk.Label(
                 self, 
                 text="Quantity of item",
                 font=label_font).pack(pady=3)
             stock_quantity.pack(pady=3)
 
-    class PerishableOptions(tk.Frame):
+    class PerishableOptions(ttk.Frame):
         def __init__(self, parent):
             super().__init__(parent)
-            tk.Label(
+            ttk.Label(
                 self,
                 text="Expiry Date",
                 font=label_font
             ).pack(pady=3)
             global stock_expiry
-            stock_expiry = tk.Entry(self, width=32, font=default_font)
+            stock_expiry = ttk.Entry(self, width=32, font=default_font)
             stock_expiry.pack(pady=3)
-            tk.Label(
+            ttk.Label(
                 self,
                 text="Storage Temperature",
                 font=label_font
             ).pack(pady=3)
             global stock_temp
-            stock_temp = tk.Entry(self, width=16, font=default_font)
+            stock_temp = ttk.Entry(self, width=16, font=default_font)
             stock_temp.pack(pady=3,side=tk.LEFT)
             global temp_value
             temp_value = tk.StringVar(value="Celsius (°C)")
             temp_options = ("Celsius (°C)","Fahrenheit (°F)")
-            temp_unit = tk.OptionMenu(self, temp_value, *temp_options)
+            temp_unit = ttk.OptionMenu(self, temp_value, *temp_options)
             temp_unit.config(width=16)
             temp_unit.pack(pady=3,side=tk.RIGHT)
 
-    class ElectronicOptions(tk.Frame):
+    class ElectronicOptions(ttk.Frame):
         def __init__(self, parent):
             super().__init__(parent)
-            tk.Label(
+            ttk.Label(
                 self,
                 text="Warranty Period", 
                 font=label_font
             ).pack(pady=3)
             global stock_warranty
-            stock_warranty = tk.Entry(self, width=32, font=default_font)
+            stock_warranty = ttk.Entry(self, width=32, font=default_font)
             stock_warranty.pack(pady=3)
-            tk.Label(
+            ttk.Label(
                 self,
                 text="Power Usage",
                 font=label_font
             ).pack(pady=3)
             global stock_power
-            stock_power = tk.Entry(self, width=32, font=default_font)
+            stock_power = ttk.Entry(self, width=32, font=default_font)
             stock_power.pack(pady=3)
 
     def update_options(selected):
@@ -165,12 +166,12 @@ def add_stock_window():
     perishable_opts = PerishableOptions(add_window)
     electronic_opts = ElectronicOptions(add_window)
 
-    tk.Label(
+    ttk.Label(
         add_window, 
         text="Add a new item",
         font=label_font).grid(row=0,column=1,columnspan=2)
     
-    tk.Label(
+    ttk.Label(
         add_window,
         text="Select a Product Type",
         font=label_font
@@ -178,16 +179,16 @@ def add_stock_window():
 
     #Allows user to choose which type of product they want to add
     options = ["Default Product", "Perishable Product", "Electronic Product"]
-    stock_type = tk.OptionMenu(add_window, opt, *options, command=update_options)
+    stock_type = ttk.OptionMenu(add_window, opt, *options, command=update_options)
     stock_type.grid(row=2,column=1,columnspan=2)
 
     DefaultOptions(add_window).grid(row=3,column=1,columnspan=2)
 
     global add_status
-    add_status = tk.Label(add_window, text="", font=("Arial",10))
+    add_status = ttk.Label(add_window, text="", font=("Arial",10))
     add_status.grid(row=5,column=1,columnspan=2)
 
-    add_button = tk.Button(add_window, text="Add Stock", command=add_stock).grid(row=6,column=1,columnspan=2)
+    add_button = ttk.Button(add_window, text="Add Stock", command=add_stock).grid(row=6,column=1,columnspan=2)
     
     #Runs the rest of the add_stock function on button click, then closes the window
 
@@ -213,25 +214,25 @@ def add_stock():
         item_quantity = stock_quantity.get().strip()
         
         if not item_name:
-            add_status.config(text="Please enter a stock item name", fg= "red")
+            add_status.config(text="Please enter a stock item name", foreground= "red")
             stock_name.focus_set()
             return
         if not item_price:
-            add_status.config(text="Please enter a stock item price.", fg= "red")
+            add_status.config(text="Please enter a stock item price.", foreground= "red")
             stock_price.focus_set()
             return
         if not item_quantity:
-            add_status.config(text="Please enter a stock item quantity.", fg= "red")
+            add_status.config(text="Please enter a stock item quantity.", foreground= "red")
             stock_quantity.focus_set()
             return
         try:
             item_quantity = int(item_quantity)
             if item_quantity < 0:
-                add_status.config(text="Please enter a quantity of 0 or larger.", fg="red")
+                add_status.config(text="Please enter a quantity of 0 or larger.", foreground="red")
                 stock_price.focus_set()
                 return
         except ValueError:
-            add_status.config(text="Please enter a whole number for stock quantity.", fg="red")
+            add_status.config(text="Please enter a whole number for stock quantity.", foreground="red")
             stock_quantity.focus_set()
             return
 
@@ -241,19 +242,19 @@ def add_stock():
             if item_price >= 0:
                 assert item_price == (round(item_price, 2))
             else: 
-                add_status.config(text="Please enter a price value of 0 or larger.", fg="red")
+                add_status.config(text="Please enter a price value of 0 or larger.", foreground="red")
                 stock_price.focus_set()
                 return
         except AssertionError:
-            add_status.config(text="Please only enter a price with up to 2 decimal points.", fg="red")
+            add_status.config(text="Please only enter a price with up to 2 decimal points.", foreground="red")
             stock_price.focus_set()
             return
         except ValueError:
-            add_status.config(text="Please only enter an integer/decimal number for the price.", fg="red")
+            add_status.config(text="Please only enter an integer/decimal number for the price.", foreground="red")
             stock_price.focus_set()
             return
         except:
-            add_status.config(text="The price entered is not possible.", fg="red")
+            add_status.config(text="The price entered is not possible.", foreground="red")
             stock_price.focus_set()
             return
 
@@ -268,11 +269,11 @@ def add_stock():
             item_temp = stock_temp.get().strip()
             temp_unit = change_temp_unit(temp_value.get())
             if not item_expiry:
-                add_status.config(text="Please enter a stock item expiry date.", fg= "red")
+                add_status.config(text="Please enter a stock item expiry date.", foreground= "red")
                 stock_expiry.focus_set()
                 return
             if not item_temp:
-                add_status.config(text="Please enter a storage temperature.", fg= "red")
+                add_status.config(text="Please enter a storage temperature.", foreground= "red")
                 stock_temp.focus_set()
                 return
             stock = Perishable(item_id,item_name,item_price,item_quantity,item_expiry,item_temp)
@@ -283,11 +284,11 @@ def add_stock():
             item_warranty = stock_warranty.get().strip()
             power_usage = stock_power.get().strip()
             if not item_warranty:
-                add_status.config(text="Please enter a stock item warranty period.", fg= "red")
+                add_status.config(text="Please enter a stock item warranty period.", foreground= "red")
                 stock_warranty.focus_set()
                 return
             if not power_usage:
-                add_status.config(text="Please enter a stock item power usage.", fg= "red")
+                add_status.config(text="Please enter a stock item power usage.", foreground= "red")
                 stock_power.focus_set()
                 return
             stock = Electronic(item_id,item_name,item_price,item_quantity,item_warranty,power_usage)
@@ -296,24 +297,24 @@ def add_stock():
         counter.increment()
 
         stock.low_stock_warning()
-        add_status.config(text="Stock Added!", fg="green")
+        add_status.config(text="Stock Added!", foreground="green")
     except tk.TclError:
-        status.config(text="An error occurred. Please close all 'Add Stock' windows and try again.",fg="red")
+        status.config(text="An error occurred. Please close all 'Add Stock' windows and try again.",foreground="red")
 
 # Add event to logs
     writeLog(f"Added item: {item_name}")
 
 #Button for adding stock to the list.
     
-add_window_button = tk.Button(btn_frame, text="Add Stock", command=add_stock_window).grid(row=0, column=0, padx=3)
+add_window_button = ttk.Button(btn_frame, text="Add Stock", command=add_stock_window, width=20).grid(row=0, column=0, padx=3)
 
-frame = tk.Frame(root, width=40, height=15)
+frame = ttk.Frame(root, width=40, height=15)
 frame.pack(pady=10)
 
-scrollbar = tk.Scrollbar(frame, orient="vertical")
+scrollbar = ttk.Scrollbar(frame, orient="vertical")
 scrollbar.pack(side="right", fill="y")
 
-sidescroll = tk.Scrollbar(frame, orient="horizontal")
+sidescroll = ttk.Scrollbar(frame, orient="horizontal")
 sidescroll.pack(side="bottom", fill="x")
 
 #Listbox to display stock items
@@ -329,17 +330,19 @@ def remove_stock():
     selected = stock_list.curselection()
     if not selected:
         status.config(text="Please select a stock item to remove from the list.",
-                      fg= "red")
+                      foreground= "red")
         return
     stock_list.delete(selected)
     status.config(text="Stock has been removed from the list.",
-                  fg="green")
+                  foreground="green")
 
 # Add event to logs
     writeLog(f"Removed item: {item_name}")
-    
+
+def edit_stock():
+    pass
+
 #Button to remove selected stock from list
-remove_button = tk.Button(btn_frame, text= "Remove stock", command=remove_stock) .grid(row=0, column=1, padx=3)  
 
 #function for editing stock here
 
@@ -389,14 +392,14 @@ def saveToFile(event=None):
 
 # Empty inventory error
     if len(invList) == 0:
-        status.config(text="Inventory is currently empty. Please add stock.", fg= "red")
+        status.config(text="Inventory is currently empty. Please add stock.", foreground= "red")
         return
 
 # Converts list type inventory to json, writes to inventory.json
     invData = json.dumps(invList, indent=4)
     with open(curSavePath, "w") as inv:
         inv.write(invData)
-    status.config(text="Inventory saved to [curSavePath] in program folder...", fg="green")
+    status.config(text="Inventory saved to [curSavePath] in program folder...", foreground="green")
 # Add event to logs
     writeLog(f"Saved inventory to [{curSavePath}]")
 
@@ -415,7 +418,7 @@ def saveAsFile(event=None):
 
 # Empty inventory error
     if len(invList) == 0:
-        status.config(text="Inventory is currently empty. Please add stock.", fg= "red")
+        status.config(text="Inventory is currently empty. Please add stock.", foreground= "red")
         return
 
 # Asks user to set a save location in new window, defaults to JSON file format
@@ -441,7 +444,7 @@ def saveAsFile(event=None):
     invData = json.dumps(invList, indent=4)
     with open(curSavePath, "w") as inv:
         inv.write(invData)
-    status.config(text=f"Inventory saved to [{saveLocation}]", fg="green")
+    status.config(text=f"Inventory saved to [{saveLocation}]", foreground="green")
 
 def loadDefaultInventory(event=None):
     global curSavePath
@@ -460,9 +463,9 @@ def loadDefaultInventory(event=None):
 
 # Reset working inventory to default one
         curSavePath = "inventorySave.json"
-        status.config(text="Default inventory loaded from program folder", fg="green")
+        status.config(text="Default inventory loaded from program folder", foreground="green")
     except FileNotFoundError:
-        status.config(text="Failed to load default inventory. Please Save one first.)", fg="red")
+        status.config(text="Failed to load default inventory. Please Save one first.)", foreground="red")
 # Add event to logs
     writeLog("Loaded default inventory [inventorySave.json]")
 
@@ -493,9 +496,9 @@ def loadFromFile():
 # Change working inventory to loaded one, so Save function overwrites this instead of Default inv
         curSavePath = loadLocation
 
-        status.config(text=f"Inventory loaded from chosen folder. [{loadLocation}]", fg="green")
+        status.config(text=f"Inventory loaded from chosen folder. [{loadLocation}]", foreground="green")
     except (FileNotFoundError, json.JSONDecodeError):
-        status.config(text="Failed to load inventory. Inventory may be corrupted, or chosen file is not valid JSON.)", fg="red")
+        status.config(text="Failed to load inventory. Inventory may be corrupted, or chosen file is not valid JSON.)", foreground="red")
 # Add event to logs
     writeLog(f"Loaded inventory from [{loadLocation}]")
 
@@ -524,13 +527,13 @@ def checkLogs():
     log_window.title("Transaction History")
     log_window.geometry("520x400")
 
-    tk.Label(log_window, text="Transaction History", font=("Arial", 12, "bold")).pack(pady=(10, 5))
+    ttk.Label(log_window, text="Transaction History", font=("Arial", 12, "bold")).pack(pady=(10, 5))
 
     # Scrollable text box
-    frame = tk.Frame(log_window)
+    frame = ttk.Frame(log_window)
     frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=5)
 
-    scrollbar = tk.Scrollbar(frame)
+    scrollbar = ttk.Scrollbar(frame)
     scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
     log_box = tk.Text(frame, yscrollcommand=scrollbar.set, state=tk.DISABLED, wrap=tk.WORD)
@@ -547,23 +550,26 @@ def checkLogs():
     log_box.config(state=tk.DISABLED)
     log_box.see(tk.END)  # scroll to most recent
 
-    tk.Button(log_window, text="Close", command=log_window.destroy).pack(pady=8)
+    ttk.Button(log_window, text="Close", command=log_window.destroy).pack(pady=8)
+
+edit_button = ttk.Button(btn_frame, text="Edit stock", command=edit_stock, width=20).grid(row=0, column=1, padx=3)
+remove_button = ttk.Button(btn_frame, text= "Remove stock", command=remove_stock, width=20) .grid(row=1, column=0, padx=3)  
 
 # Creates the Save button, separately assigns it to grid and binds function
-save_button = tk.Button(btn_frame, text= "Save inventory")
+save_button = ttk.Button(btn_frame, text= "Save inventory", width=20)
 save_button.grid(row=2, column=0, padx=3)
 save_button.bind("<Button-1>", saveToFile)
 
 # Creates Load button, separately assigns to grid
-load_button = tk.Button(btn_frame, text= "Load inventory")
+load_button = ttk.Button(btn_frame, text= "Load inventory", width=20)
 load_button.grid(row=2, column=1, padx=3)
 load_button.bind("<Button-1>", loadDefaultInventory)
 
 # Creates "Check Logs" button, separately assigns to grid
-check_logs = tk.Button(btn_frame, text= "Transaction history", command=checkLogs)
-check_logs.grid(row=3, column=0, padx=3)
+check_logs = ttk.Button(btn_frame, text= "Transaction history", command=checkLogs, width=20)
+check_logs.grid(row=1, column=1, padx=3)
 
-tipLabel=tk.Label(root, text="Hold 'Shift' key to show alternative save and load options", font=("Arial",10))
+tipLabel=ttk.Label(root, text="Hold 'Shift' key to show alternative save and load options", font=("Arial",10))
 tipLabel.pack()
 
 # This binds the Shift key press event to the whole window with a lambda function, swaps Save and Load
